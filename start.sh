3b0bfab009efb432048650d00ad162673cfd5ed4
gmgarcia816 <69204381+gmgarcia816@users.noreply.github.com>
2021-06-20 01:31:36 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt install nodejs
+npm install --prefix ./backend
+npm install --prefix ./frontend
+cd backend
+(npm run start&)
+cd
+cd devops-challenge
+cd frontend
+(npm run start&)
