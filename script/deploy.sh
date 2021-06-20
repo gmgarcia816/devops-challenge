7aa65e8cfb0f08df831b9d36ed6826e07d36ff72
gmgarcia816 <69204381+gmgarcia816@users.noreply.github.com>
2021-06-20 15:29:22 -0400

    Delete script directory

--- script/deploy.sh
+++
@@ -1,6 +0,0 @@
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt install nodejs
-npm install --prefix ./backend
-npm install --prefix ./frontend
-npm run build --prefix ./frontend
-npm install -g serve
