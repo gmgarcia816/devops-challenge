1361d2b9c45d3fe1cd8321b6bb01d0e87d9f6510
gmgarcia816 <69204381+gmgarcia816@users.noreply.github.com>
2021-06-20 14:54:42 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt install nodejs
 npm install --prefix ./backend
 npm install --prefix ./frontend
-
+npm run build --prefix ./frontend
+npm install -g serve
